d5f5f27c77633477fc940f61a39c50cf5f8339d4
mahamudigtal <alexandremahmud@hotmail.com>
2022-06-09 12:21:49 -0300

    Codificação do projeto, finalização

--- app/src/main/java/com/mahamudigitallab/frasesdodia/MainActivity.java
+++ app/src/main/java/com/mahamudigitallab/frasesdodia/MainActivity.java
@@ -3,6 +3,10 @@ package com.mahamudigitallab.frasesdodia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void gerarNovaFrase(View view){
+
+        String[] frases = {
+                "Nunca se afaste de seus sonhos porque se eles se forem , você estará vivendo mas terá deixado de existir",
+                "Deus ajuda quem cedo madruga, acorda vagabundo!",
+                "Falar das pinga que eu tomo é fácil, dificil é fazer o que eu faço!",
+                "Nunca é tarde para começar!",
+                "O sucesso não tem limite"
+
+        };
+
+        int numero = new Random().nextInt(5);
+
+        TextView texto = findViewById(R.id.TextResultado);
+        texto.setText(frases[numero]);
     }
 }
